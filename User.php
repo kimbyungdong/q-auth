ea64b6a1486ff1b66bb372871f6903ba37d3229e
security next <sec-team@nextit.jp>
2020-01-30 16:46:07 +0900

    20111

--- User.php
+++ User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','area','experience',
+        'name', 'email', 'password','area','experience','file',
     ];
 
     /**
